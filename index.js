ec16c526eef8b68764d66488d3bc7bdaefc9629b
Anatoly Tarnavsky <anatolyt@gmail.com>
2019-05-26 22:20:39 +0300

    All workflow can be managed from config.json

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+const config = require("./config");
 const dsl2anki = require("./dsl2anki");
 const markdown2anki = require("./markdown2anki");
-markdown2anki();
-// dsl2anki();
+
+if (config.mode === "dsl2anki"){
+    dsl2anki();
+}else if (config.mode === "markdown2anki"){
+    markdown2anki();
+}else {
+    console.error("Wrong mode, possible values are 'dsl2anki' or 'markdown2anki'");
+}
+
